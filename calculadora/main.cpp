734d9b49d770eefb8d1c8d7fec748a631217419c
Jhonathan Otamendi <jhonathanguerrero030@gmail.com>
2024-06-23 11:12:42 -0400

    Version 1.1

--- calculadora/main.cpp
+++ calculadora/main.cpp
@@ -1,9 +1,29 @@
-//qlq dice esa plantilla para el flojoooo
+//calculadora 1.0//
+
+#include <iostream>
+using namespace std;
+
+int suma(int a, int b) {
+	return a + b;
+}
+int resta(int a, int b) {
+	return a - b;
+}
+
+void calcular(int(*funcion)(int, int), int val_a, int val_b) {
+	int result;
+	result = funcion(val_a, val_b);
+	cout << "resultado = " << result << endl;
+}
 
 
 int main() {
+	int(*var_funcion)(int, int);
+	var_funcion = suma;
+	calcular(var_funcion, 10, 20);
+	var_funcion = resta;
+	calcular(var_funcion, 10, 20);
 
-
-
+	cin.get();
 	return 0;
 }
