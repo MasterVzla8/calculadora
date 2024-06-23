ae65915ab15b1c4b4b141a880be17d05fb0e3f15
Jhonathan Otamendi <jhonathanguerrero030@gmail.com>
2024-06-23 11:16:10 -0400

    Version 1.3

--- calculadora/main.cpp
+++ calculadora/main.cpp
@@ -9,6 +9,12 @@ int suma(int a, int b) {
 int resta(int a, int b) {
 	return a - b;
 }
+int multi(int a, int b) {
+	return a * b;
+}
+int divi(int a, int b) {
+	return a / b;
+}
 
 void calcular(int(*funcion)(int, int), int val_a, int val_b) {
 	int result;
@@ -23,6 +29,10 @@ int main() {
 	calcular(var_funcion, 10, 20);
 	var_funcion = resta;
 	calcular(var_funcion, 10, 20);
+	var_funcion = multi;
+	calcular(var_funcion, 10, 20);
+	var_funcion = divi;
+	calcular(var_funcion, 10, 2);
 
 	cin.get();
 	return 0;
